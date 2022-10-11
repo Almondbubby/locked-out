cd64b9b1a09f77db53a15f1f8f8f4a28fd1afb6b
root <root@DESKTOP-GPE14EH.localdomain>
2022-10-11 18:31:06 -0400

    added animations

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,15 +9,31 @@ public class CharacterMovement : MonoBehaviour
     private float movementSpeed;
     private Vector2 movement;
     private Rigidbody2D rbody;
+    private Animator anim;
  
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
+        if(movement.y > 0){
+            anim.Play("Player_Walk_Forwards");
+        }
+        if(movement.y < 0){
+            anim.Play("Player_Walk_Back");
+        }
+        if(movement.y == 0){
+            if(movement.x < 0){
+                anim.Play("Player_Walk_Left");
+            }
+            if(movement.x > 0){
+                anim.Play("Player_Walk_Right");
+            }
+        }
     }
 
     public void Movement()
